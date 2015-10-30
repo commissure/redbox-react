6b44f19fd47242eafd297c9259addd21df72eae9
stevo <stevoland@gmail.com>
2015-10-30 16:09:35 +0000

    feat(links): open files in configured editor
    If filenames are absolute the generated links can be opened
    in an editor if the `editorScheme` property is set.
    
    If `react-transform-catch-errors` passes in a `filename` this
    is used for the top function if the stack trace filename
    isn't absolute
    
    Closes: #10

--- src/index.js
+++ src/index.js
@@ -2,23 +2,44 @@ import React, {Component, PropTypes} from 'react'
 import style from './style.js'
 import ErrorStackParser from 'error-stack-parser'
 import assign from 'object-assign'
+import {isFilenameAbsolute, makeUrl, makeLinkText} from './lib'
 
 export default class RedBox extends Component {
   static propTypes = {
-    error: PropTypes.instanceOf(Error).isRequired
+    error: PropTypes.instanceOf(Error).isRequired,
+    filename: PropTypes.string,
+    editorScheme: PropTypes.string,
+    useLines: PropTypes.bool,
+    useColumns: PropTypes.bool
   }
   static displayName = 'RedBox'
+  static defaultProps = {
+    useLines: true,
+    useColumns: true
+  }
   render () {
-    const {error} = this.props
+    const {error, filename, editorScheme, useLines, useColumns} = this.props
     const {redbox, message, stack, frame, file, linkToFile} = assign({}, style, this.props.style)
 
     const frames = ErrorStackParser.parse(error).map((f, index) => {
-      const link = `${f.fileName}:${f.lineNumber}:${f.columnNumber}`
+      let text
+      let url
+
+      if (index === 0 && filename && !isFilenameAbsolute(f.fileName)) {
+        url = makeUrl(filename, editorScheme)
+        text = makeLinkText(filename)
+      } else {
+        let lines = useLines ? f.lineNumber : null
+        let columns = useColumns ? f.columnNumber : null
+        url = makeUrl(f.fileName, editorScheme, lines, columns)
+        text = makeLinkText(f.fileName, lines, columns)
+      }
+
       return (
         <div style={frame} key={index}>
           <div>{f.functionName}</div>
           <div style={file}>
-            <a href={link} style={linkToFile}>{link}</a>
+            <a href={url} style={linkToFile}>{text}</a>
           </div>
         </div>
       )
--- tests/index.js
+++ tests/index.js
@@ -1,18 +1,36 @@
+import React from 'react'
 import test from 'tape'
 import {createComponent} from './utils'
+import errorStackParserMock from './errorStackParserMock'
+import framesStub from './framesStub.json'
+import framesStubAbsoluteFilenames from './framesStubAbsoluteFilenames.json'
 import RedBox from '../src'
+import style from '../src/style'
+import './lib';
+
+
+const beforeEach = (framesStub) => {
+  RedBox.__Rewire__('ErrorStackParser', errorStackParserMock(framesStub))
+}
+
+const afterEach = () => {
+  RedBox.__ResetDependency__('ErrorStackParser')
+}
 
 test('RedBox static displayName', t => {
   t.plan(1)
+  beforeEach(framesStub)
   t.equal(
     RedBox.displayName,
     'RedBox',
     'correct static displayName property on class'
   )
+  afterEach()
 })
 
 test('RedBox error message', t => {
   t.plan(3)
+  beforeEach(framesStub)
   const ERR_MESSAGE = 'funny error name'
   const error = new Error(ERR_MESSAGE)
   const component = createComponent(RedBox, {error})
@@ -35,6 +53,191 @@ test('RedBox error message', t => {
     ERR_MESSAGE,
     'Main error message ends with message originally supplied to error constructor.'
   )
+  afterEach()
 })
 
-// TODO: Tests for the stack frame rendering
+test('RedBox stack trace', t => {
+  t.plan(1)
+  beforeEach(framesStub)
+  const error = new Error()
+  const component = createComponent(RedBox, {error})
+
+  const renderedStack = component
+    .props.children[1]
+
+  t.deepEqual(
+    renderedStack,
+    <div style={style.stack}>
+      <div style={style.frame} key={0}>
+        <div>App.render</div>
+        <div style={style.file}>
+          <a style={style.linkToFile} href="webpack:///./components/App.js?">webpack:///./components/App.js?:45:12</a>
+        </div>
+      </div>
+      <div style={style.frame} key={1}>
+        <div>App.render</div>
+        <div style={style.file}>
+          <a style={style.linkToFile} href="webpack:///./~/react-hot-loader/~/react-hot-api/modules/makeAssimilatePrototype.js?">webpack:///./~/react-hot-loader/~/react-hot-api/modules/makeAssimilatePrototype.js?:17:41</a>
+        </div>
+      </div>
+    </div>
+  )
+
+  afterEach()
+})
+
+test('RedBox with filename from react-transform-catch-errors', t => {
+  t.plan(1)
+  beforeEach(framesStub)
+  const error = new Error()
+  const filename = 'some-optional-webpack-loader!/filename'
+  const component = createComponent(RedBox, {error, filename})
+
+  const renderedStack = component
+    .props.children[1]
+
+  t.deepEqual(
+    renderedStack,
+    <div style={style.stack}>
+      <div style={style.frame} key={0}>
+        <div>App.render</div>
+        <div style={style.file}>
+          <a style={style.linkToFile} href="file:///filename">/filename</a>
+        </div>
+      </div>
+      <div style={style.frame} key={1}>
+        <div>App.render</div>
+        <div style={style.file}>
+          <a style={style.linkToFile} href="webpack:///./~/react-hot-loader/~/react-hot-api/modules/makeAssimilatePrototype.js?">webpack:///./~/react-hot-loader/~/react-hot-api/modules/makeAssimilatePrototype.js?:17:41</a>
+        </div>
+      </div>
+    </div>
+  )
+  afterEach()
+})
+
+test('RedBox with filename and editorScheme', t => {
+  t.plan(1)
+  beforeEach(framesStub)
+  const error = new Error()
+  const filename = 'some-optional-webpack-loader!/filename'
+  const editorScheme = 'subl'
+  const component = createComponent(RedBox, {error, filename, editorScheme})
+
+  const renderedStack = component
+    .props.children[1]
+
+  t.deepEqual(
+    renderedStack,
+    <div style={style.stack}>
+      <div style={style.frame} key={0}>
+        <div>App.render</div>
+        <div style={style.file}>
+          <a style={style.linkToFile} href="subl://open?url=file:///filename">/filename</a>
+        </div>
+      </div>
+      <div style={style.frame} key={1}>
+        <div>App.render</div>
+        <div style={style.file}>
+          <a style={style.linkToFile} href="webpack:///./~/react-hot-loader/~/react-hot-api/modules/makeAssimilatePrototype.js?">webpack:///./~/react-hot-loader/~/react-hot-api/modules/makeAssimilatePrototype.js?:17:41</a>
+        </div>
+      </div>
+    </div>
+  )
+  afterEach()
+})
+
+test('RedBox with absolute filenames', t => {
+  t.plan(1)
+  beforeEach(framesStubAbsoluteFilenames)
+  const error = new Error()
+  const filename = 'some-optional-webpack-loader!/filename'
+  const editorScheme = 'subl'
+  const component = createComponent(RedBox, {error, filename, editorScheme})
+
+  const renderedStack = component
+    .props.children[1]
+
+  t.deepEqual(
+    renderedStack,
+    <div style={style.stack}>
+      <div style={style.frame} key={0}>
+        <div>App.render</div>
+        <div style={style.file}>
+          <a style={style.linkToFile} href="subl://open?url=file:///components/App.js&line=45&column=12">/components/App.js:45:12</a>
+        </div>
+      </div>
+      <div style={style.frame} key={1}>
+        <div>App.render</div>
+        <div style={style.file}>
+          <a style={style.linkToFile} href="subl://open?url=file:///some-path/modules/makeAssimilatePrototype.js&line=17&column=41">/some-path/modules/makeAssimilatePrototype.js:17:41</a>
+        </div>
+      </div>
+    </div>
+  )
+  afterEach()
+})
+
+test('RedBox with absolute filenames but unreliable line numbers', t => {
+  t.plan(1)
+  beforeEach(framesStubAbsoluteFilenames)
+  const error = new Error()
+  const filename = 'some-optional-webpack-loader!/filename'
+  const editorScheme = 'subl'
+  const useLines = false
+  const component = createComponent(RedBox, {error, filename, editorScheme, useLines})
+
+  const renderedStack = component
+    .props.children[1]
+
+  t.deepEqual(
+    renderedStack,
+    <div style={style.stack}>
+      <div style={style.frame} key={0}>
+        <div>App.render</div>
+        <div style={style.file}>
+          <a style={style.linkToFile} href="subl://open?url=file:///components/App.js">/components/App.js</a>
+        </div>
+      </div>
+      <div style={style.frame} key={1}>
+        <div>App.render</div>
+        <div style={style.file}>
+          <a style={style.linkToFile} href="subl://open?url=file:///some-path/modules/makeAssimilatePrototype.js">/some-path/modules/makeAssimilatePrototype.js</a>
+        </div>
+      </div>
+    </div>
+  )
+  afterEach()
+})
+
+test('RedBox with absolute filenames but unreliable column numbers', t => {
+  t.plan(1)
+  beforeEach(framesStubAbsoluteFilenames)
+  const error = new Error()
+  const filename = 'some-optional-webpack-loader!/filename'
+  const editorScheme = 'subl'
+  const useColumns = false
+  const component = createComponent(RedBox, {error, filename, editorScheme, useColumns})
+
+  const renderedStack = component
+    .props.children[1]
+
+  t.deepEqual(
+    renderedStack,
+    <div style={style.stack}>
+      <div style={style.frame} key={0}>
+        <div>App.render</div>
+        <div style={style.file}>
+          <a style={style.linkToFile} href="subl://open?url=file:///components/App.js&line=45">/components/App.js:45</a>
+        </div>
+      </div>
+      <div style={style.frame} key={1}>
+        <div>App.render</div>
+        <div style={style.file}>
+          <a style={style.linkToFile} href="subl://open?url=file:///some-path/modules/makeAssimilatePrototype.js&line=17">/some-path/modules/makeAssimilatePrototype.js:17</a>
+        </div>
+      </div>
+    </div>
+  )
+  afterEach()
+})
